ccc08b4428d6f4da3acd6b7aed817323df59511a
Jon Buda <jon.buda@gmail.com>
2011-02-12 17:57:28 -0600

    remove 'mobile' from user agents since it targets ipad as well (mobile safari)

--- lib/mobilized.rb
+++ lib/mobilized.rb
@@ -5,8 +5,8 @@ module Mobilized
                         'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
                         'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
                         'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                        'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|' +
-                        'mobile'
+                        'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer'
+                        # '|mobile' - targets ipad as well.
                         # |ipad - removed for now.
   module ClassMethods
     def mobilized
